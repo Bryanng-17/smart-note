2524cea3d5080894d44e5ef369fad35a96c4fe59
bryan <huangm871@gmail.com>
2025-10-24 20:41:47 +0800

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,22 +4,24 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
-
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
+const __dirname = path.resolve();
 //npm install nodemon -D 
 //upper is command to restart without manually run again the terminal
 //In development, run node run dev
 //In deployment, run node run start
 
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }));
+}
 
-app.use(cors({
-    origin:"http://localhost:5173",
-}));
 //middleware to allow to get access of the request body
 app.use(express.json());//this middleware will parse JSON bodies:req.body
 app.use(rateLimiter);
@@ -30,6 +32,15 @@ app.use(rateLimiter);
 //     next();
 // });
 app.use("/api/notes", router);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
 
 connectDB().then(() => {
     app.listen(PORT, () => {
